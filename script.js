5222eb88afadae90b40b7266a59c68d30bda34d6
Daniel Sherlock <d.t.sherlock@live.co.uk>
2019-06-08 23:20:45 +0100

    eurgh no symbols, fixed the switch statement (ugly though)

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function main() {
   // probably some event handlers need registering
   // read the table, somehow
   const params = (new URL(document.location)).searchParams;
-  switch ([params.get('source'), params.has('cached')]) {
+  switch ([params.get('source'), params.has('cached')].join(' ')) {
     default:
-    case ['betdata', false]:
+    case 'betdata false':
       if (window.confirm("betdata request succeeded")) {
         console.log("betdata request suceeded");
         // get from betdata
@@ -13,14 +13,14 @@ function main() {
         console.log("betdata request failed");
         // add some kind of message that the request failed
       }
-    case ['betdata', true]:
+    case 'betdata true':
       console.log("betdata cache accessed");
       // read from file
       break;
-    case ['predictit', true]:
+    case 'predictit true':
       console.log("predictit cache failed");
       // show message that we do not cache predictit data
-    case ['predictit', false]:
+    case 'predictit false':
       console.log("predictit request");
       // get from predictit
       break;
